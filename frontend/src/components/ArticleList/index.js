89f9376d8f6e937a3763868d2cdd799d759c4027
Son Hye Joo <seongwoo.cho@gmail.com>
2023-04-05 14:18:27 -0400

    singleArticle fixed

--- frontend/src/components/ArticleList/index.js
+++ frontend/src/components/ArticleList/index.js
@@ -1,15 +1,15 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Route, Switch, NavLink } from 'react-router-dom';
-import SingleArticle from '../SingleArticle';
-import { loadArticles } from '../../store/articleReducer';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch, NavLink } from "react-router-dom";
+import SingleArticle from "../SingleArticle";
+import { fetchArticles } from "../../store/articleReducer";
 
 const ArticleList = () => {
   const dispatch = useDispatch();
-  const articles = useSelector(state=>state.articleState.entries);
+  const articles = useSelector((state) => state.articleState.entries);
 
   useEffect(() => {
-    dispatch(loadArticles());
+    dispatch(fetchArticles());
   }, [dispatch]);
 
   return (
@@ -17,12 +17,14 @@ const ArticleList = () => {
       <h1>Article List</h1>
       <ol>
         {articles.map(({ id, title }) => (
-          <li key={id}><NavLink to={`/article/${id}`}>{title}</NavLink></li>
+          <li key={id}>
+            <NavLink to={`/article/${id}`}>{title}</NavLink>
+          </li>
         ))}
       </ol>
 
       <Switch>
-        <Route path='/article/:id'>
+        <Route path="/article/:id">
           <SingleArticle articles={articles} />
         </Route>
       </Switch>
--- frontend/src/components/SingleArticle/index.js
+++ frontend/src/components/SingleArticle/index.js
@@ -1,19 +1,17 @@
-import { useParams } from 'react-router-dom';
-import './SingleArticle.css';
+import { useParams } from "react-router-dom";
+import "./SingleArticle.css";
 
 const SingleArticle = ({ articles }) => {
   const { id } = useParams();
-  const singleArticle = articles.find(article => article.id === id);
+  const singleArticle = articles.find((article) => article.id === parseInt(id));
+  if (!singleArticle) {
+    return null;
+  }
   return (
-    <div className='singleArticle'>
+    <div className="singleArticle">
       <h1>{singleArticle.title}</h1>
-      <img
-        src={singleArticle.imageUrl}
-        alt={singleArticle.title}
-      />
-      <p>
-        {singleArticle.body}
-      </p>
+      <img src={singleArticle.imageUrl} alt={singleArticle.title} />
+      <p>{singleArticle.body}</p>
     </div>
   );
 };
