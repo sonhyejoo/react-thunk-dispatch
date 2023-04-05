56e037b4aa1dc73a3e188e6e41a5ed67f5569eac
Son Hye Joo <seongwoo.cho@gmail.com>
2023-04-05 14:37:08 -0400

    removed unnecessary nanoid

--- frontend/src/components/ArticleInput/index.js
+++ frontend/src/components/ArticleInput/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 import { writeArticle } from "../../store/articleReducer";
 import "./ArticleInput.css";
 
